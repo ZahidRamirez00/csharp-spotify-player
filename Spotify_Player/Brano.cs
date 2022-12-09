a3e384e7ac65b11aa4f27fcfa64029e57ac3f3f2
ZahidRamirez00 <zahidramirez.00@gmail.com>
2022-12-09 10:23:57 +0100

    Creazione delle classi Podcast e Brano

---
+++ Spotify_Player/Brano.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spotify_Player
+{
+    internal class Brano : IContenutoRiproducibile
+    {
+        public override string Album()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int Anno()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string Artista()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string Durata()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string Genere()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string Titolo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
