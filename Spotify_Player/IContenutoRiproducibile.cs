e12cec358a27e9b93795555d2db497c3a64cf59a
ZahidRamirez00 <zahidramirez.00@gmail.com>
2022-12-09 10:16:40 +0100

    Creazione dell'interfaccia <IContenutoRiproducibile>

---
+++ Spotify_Player/IContenutoRiproducibile.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spotify_Player
+{
+    public abstract class IContenutoRiproducibile
+    {
+        public abstract string Titolo();
+        public abstract string Artista();
+        public abstract string Album();
+        public abstract string Genere();
+        public abstract string Durata();
+        public abstract int Anno();
+
+
+        // METODI PUBBLICI
+        public void Play()
+        {
+            Console.WriteLine("Hai messo play al contenuto.");
+        }
+        public void Pausa()
+        {
+            Console.WriteLine("Hai messo in pausa il contenuto.");
+        }
+        public void Stop()
+        {
+            Console.WriteLine("Hai stoppato il contenuto.");
+        }
+    }
+}
